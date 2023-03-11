5d279286c889640ed992c4f7f8ac5c61b0b48c16
Harminder Virk <virk.officials@gmail.com>
2023-03-11 10:49:55 +0530

    style: reformat files

--- src/formatters.ts
+++ src/formatters.ts
@@ -12,42 +12,42 @@
 import moize from 'moize'
 
 export const number = moize.default(
-  (...args: ConstructorParameters<typeof Intl['NumberFormat']>) => {
+  (...args: ConstructorParameters<(typeof Intl)['NumberFormat']>) => {
     return new Intl.NumberFormat(...args)
   },
   { isDeepEqual: true }
 )
 
 export const date = moize.default(
-  (...args: ConstructorParameters<typeof Intl['DateTimeFormat']>) => {
+  (...args: ConstructorParameters<(typeof Intl)['DateTimeFormat']>) => {
     return new Intl.DateTimeFormat(...args)
   },
   { isDeepEqual: true }
 )
 
 export const plural = moize.default(
-  (...args: ConstructorParameters<typeof Intl['PluralRules']>) => {
+  (...args: ConstructorParameters<(typeof Intl)['PluralRules']>) => {
     return new Intl.PluralRules(...args)
   },
   { isDeepEqual: true }
 )
 
 export const relative = moize.default(
-  (...args: ConstructorParameters<typeof Intl['RelativeTimeFormat']>) => {
+  (...args: ConstructorParameters<(typeof Intl)['RelativeTimeFormat']>) => {
     return new Intl.RelativeTimeFormat(...args)
   },
   { isDeepEqual: true }
 )
 
 export const list = moize.default(
-  (...args: ConstructorParameters<typeof Intl['ListFormat']>) => {
+  (...args: ConstructorParameters<(typeof Intl)['ListFormat']>) => {
     return new Intl.ListFormat(...args)
   },
   { isDeepEqual: true }
 )
 
 export const displayNames = moize.default(
-  (...args: ConstructorParameters<typeof Intl['DisplayNames']>) => {
+  (...args: ConstructorParameters<(typeof Intl)['DisplayNames']>) => {
     return new Intl.DisplayNames(...args)
   },
   { isDeepEqual: true }
